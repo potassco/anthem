7e72d63663ff235cd35606d1c900e7486db7cf22
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2025-04-11 14:23:07 +0200

    Make anthem a library
    Closes #135

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-pub mod analyzing;
-pub mod breaking;
-pub mod command_line;
-pub mod convenience;
-pub mod formatting;
-pub mod parsing;
-pub mod simplifying;
-pub mod syntax_tree;
-pub mod translating;
-pub mod verifying;
-
-use anyhow::Result;
-
-fn main() -> Result<()> {
-    crate::command_line::procedures::main()
+fn main() -> anyhow::Result<()> {
+    anthem::command_line::procedures::main()
 }
