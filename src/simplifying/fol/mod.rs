15fd724ed071a47bc1c12445ac86dc6759136f4e
ZachJHansen <zachhansen@unomaha.edu>
2025-04-17 19:05:05 -0500

    forward and backward proof

--- src/simplifying/fol/mod.rs
+++ src/simplifying/fol/mod.rs
@@ -29,27 +29,32 @@ pub fn validate_simplifications(formula: Formula) -> Vec<crate::verifying::probl
         f2 = f1.clone().apply(&mut classic::remove_double_negation);
         formulas.push((f2.clone(), "remove_double_negation".to_string()));
 
-        f2 = f2.apply(&mut classic::substitute_defined_variables)
+        f2 = f2
+            .apply(&mut classic::substitute_defined_variables)
             .apply(&mut intuitionistic::remove_orphaned_variables)
             .apply(&mut intuitionistic::remove_empty_quantifications);
         formulas.push((f2.clone(), "substitute_defined_variables".to_string()));
 
-        f2 = f2.apply(&mut classic::unstable::restrict_quantifier_domain)
+        f2 = f2
+            .apply(&mut classic::unstable::restrict_quantifier_domain)
             .apply(&mut intuitionistic::remove_orphaned_variables)
             .apply(&mut intuitionistic::remove_empty_quantifications);
         formulas.push((f2.clone(), "restrict_quantifier_domain".to_string()));
 
-        f2 = f2.apply(&mut classic::unstable::extend_quantifier_scope)
+        f2 = f2
+            .apply(&mut classic::unstable::extend_quantifier_scope)
             .apply(&mut intuitionistic::remove_orphaned_variables)
             .apply(&mut intuitionistic::remove_empty_quantifications);
         formulas.push((f2.clone(), "extend_quantifier_scope".to_string()));
 
-        f2 = f2.apply(&mut classic::unstable::simplify_transitive_equality)
+        f2 = f2
+            .apply(&mut classic::unstable::simplify_transitive_equality)
             .apply(&mut intuitionistic::remove_orphaned_variables)
             .apply(&mut intuitionistic::remove_empty_quantifications);
         formulas.push((f2.clone(), "simplify_transitive_equality".to_string()));
 
-        f2 = f2.apply(&mut [INTUITIONISTIC].concat().into_iter().compose())
+        f2 = f2
+            .apply(&mut [INTUITIONISTIC].concat().into_iter().compose())
             .apply(&mut intuitionistic::remove_orphaned_variables)
             .apply(&mut intuitionistic::remove_empty_quantifications);
         formulas.push((f2.clone(), "intuitionistic".to_string()));
@@ -63,16 +68,17 @@ pub fn validate_simplifications(formula: Formula) -> Vec<crate::verifying::probl
     let mut problems = vec![];
 
     for i in 0..(formulas.len() - 1) {
-        let axiom = formulas[i].clone().0;
-        let (conjecture, simplification) = formulas[i + 1].clone();
-        let name = format!("problem_{}_{}", simplification, i);
-        let problem = Problem::with_name(name)
-            .add_theory(axiom.into(), |i, formula| AnnotatedFormula {
+        let left = formulas[i].clone();
+        let right = formulas[i + 1].clone();
+        let fname = format!("forward_{}_{}", right.1, i);
+        let bname = format!("backward_{}_{}", right.1, i);
+        let problem_left = Problem::with_name(fname)
+            .add_theory(left.0.clone().into(), |i, formula| AnnotatedFormula {
                 name: format!("axiom_{i}"),
                 role: Role::Axiom,
                 formula,
             })
-            .add_theory(conjecture.into(), |i, formula| AnnotatedFormula {
+            .add_theory(right.0.clone().into(), |i, formula| AnnotatedFormula {
                 name: format!("conjecture_{i}"),
                 role: Role::Conjecture,
                 formula,
@@ -80,7 +86,22 @@ pub fn validate_simplifications(formula: Formula) -> Vec<crate::verifying::probl
             .rename_conflicting_symbols()
             .create_unique_formula_names();
 
-        problems.push(problem);
+        let problem_right = Problem::with_name(bname)
+            .add_theory(right.0.into(), |i, formula| AnnotatedFormula {
+                name: format!("axiom_{i}"),
+                role: Role::Axiom,
+                formula,
+            })
+            .add_theory(left.0.into(), |i, formula| AnnotatedFormula {
+                name: format!("conjecture_{i}"),
+                role: Role::Conjecture,
+                formula,
+            })
+            .rename_conflicting_symbols()
+            .create_unique_formula_names();
+
+        problems.push(problem_left);
+        problems.push(problem_right);
     }
 
     problems
