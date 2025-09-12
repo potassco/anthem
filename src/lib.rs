2795e02fd7bd6e999dfd6f512d948cca2eaa6b88
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2025-09-12 18:10:49 +0200

    Make certain modules private to the crate for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub mod analyzing;
-pub mod breaking;
-pub mod command_line;
+pub(crate) mod breaking;
+pub(crate) mod command_line;
 pub mod convenience;
 pub mod formatting;
 pub mod parsing;
-pub mod simplifying;
+pub(crate) mod simplifying;
 pub mod syntax_tree;
 pub mod translating;
-pub mod verifying;
+pub(crate) mod verifying;
